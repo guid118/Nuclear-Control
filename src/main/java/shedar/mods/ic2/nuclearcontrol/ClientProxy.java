f06258e559e645ed742956172e79f33125313493
glowredman <35727266+glowredman@users.noreply.github.com>
2022-05-22 17:51:17 +0000

    Update cape URL
    See https://github.com/GTNewHorizons/Nuclear-Control/commit/94d1d1cc9a62bdceb23ef78d98e8e9d90288a6f2#commitcomment-62256800

--- src/main/java/shedar/mods/ic2/nuclearcontrol/ClientProxy.java
+++ src/main/java/shedar/mods/ic2/nuclearcontrol/ClientProxy.java
@@ -77,6 +77,6 @@ public class ClientProxy extends CommonProxy {
 	}
 
 	private void registerSupporterCapes() {
-		DevCapes.getInstance().registerConfig("http://technic-solder.eu-central-1.linodeobjects.com/cape.json");
+		DevCapes.getInstance().registerConfig("https://raw.githubusercontent.com/maggi373/files/main/capes/cape.json");
 	}
 }
